5726eb4c76411d580ae3fa78e911d6f0abea1ef3
fredgit2016 <le-gac.frederic@laposte.net>
2016-06-01 11:34:07 +0200

    gitignore + main

---
+++ src/main/java/myGreatProject/Main.java
@@ -0,0 +1,11 @@
+package myGreatProject;
+
+import com.google.common.base.Strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty("ok"));
+	}
+
+}
